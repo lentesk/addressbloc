08cb8a99e66825cbdb6c8ac1326990af50576aac
Katie <lentesk@gmail.com>
2018-08-27 20:34:08 -0500

    first try

--- address-bloc/spec/address_book_spec.rb
+++ address-bloc/spec/address_book_spec.rb
@@ -83,4 +83,31 @@ RSpec.describe AddressBook do
 	    check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 	  end
   end
+
+  describe "#import_from_csv_2" do
+  	it "imports the correct number of entries" do
+  		book.import_from_csv("entries_2.csv")
+  		book_size = book.entries.size
+
+  		expect(book_size).to eq 3
+  	end
+
+  	it "imports the 1st entry" do
+  		book.import_from_csv("entries_2.csv")
+  		entry_one = book.entries[0]
+  		check_entry(entry_one, "Sarah","555-555-1111","sarah@sarahmail.com")
+   	end
+
+	  it "imports the 2nd entry" do
+	    book.import_from_csv("entries_2.csv")
+	    entry_two = book.entries[1]
+	    check_entry(entry_two, "Meg","555-555-2222","meg@megmail.com")
+	  end
+
+	  it "imports the 3rd entry" do
+	    book.import_from_csv("entries_2.csv")
+	    entry_three = book.entries[2]
+	    check_entry(entry_three, "Taylor","555-555-3333","taylor@taylor.com")
+	  end
+  end
 end
